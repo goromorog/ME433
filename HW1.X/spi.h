7d7cb1a38f48c66c5a42851f40b3c62708c54050
goromorog <zhangkeene@gmail.com>
2019-04-15 13:37:31 -0500

    Create spi.h

---
+++ HW1.X/spi.h
@@ -0,0 +1,12 @@
+#ifndef _SPI_H    /* Guard against multiple inclusion */
+#define _SPI_H
+
+void setVoltage(char channel, int voltage);
+void initSPI1();
+char SPI1_IO(char write)
+
+
+
+#endif 
+
+
