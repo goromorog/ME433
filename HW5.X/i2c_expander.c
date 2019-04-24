9274153643fede880e19bc56ff6519fcce47ca5d
goromorog <zhangkeene@gmail.com>
2019-04-23 21:43:16 -0500

    final hw5

--- HW5.X/i2c_expander.c
+++ HW5.X/i2c_expander.c
@@ -20,7 +20,7 @@ void setExpander(char pin, char level){
     //per Nick, we're just setting one pin, so ignore reading and just writing
     
     i2c_master_start();
-    i2c_master_send(01000000);//A0=0, A1=0, A2=0, writing
+    i2c_master_send(0b01000000);//A0=0, A1=0, A2=0, writing
     i2c_master_send(0x0A); //OLAT ADDR 0x0A
     i2c_master_send(level << pin);
     i2c_master_stop();
@@ -28,10 +28,10 @@ void setExpander(char pin, char level){
 
 char getExpander(){
     i2c_master_start();
-    i2c_master_send(01000000); //A0=0, A1=0, A2=0, writing
+    i2c_master_send(0b01000000); //A0=0, A1=0, A2=0, writing
     i2c_master_send(0x09); //GPIO ADDR 0x09
     i2c_master_restart();
-    i2c_master_send(01000001); //A0=0, A1=0, A2=0, reading
+    i2c_master_send(0b01000001); //A0=0, A1=0, A2=0, reading
     char r = i2c_master_recv(); //save returned value
     i2c_master_ack(1);
     i2c_master_stop();
