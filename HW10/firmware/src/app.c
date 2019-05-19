5781dd5832358f5f4e79efd7bde26a6f681ed78d
goromorog <zhangkeene@gmail.com>
2019-05-19 13:45:30 -0500

    imu data sent to computer

--- HW10/firmware/src/app.c
+++ HW10/firmware/src/app.c
@@ -449,22 +449,225 @@ void APP_Tasks(void) {
             /* PUT THE TEXT YOU WANT TO SEND TO THE COMPUTER IN dataOut
             AND REMEMBER THE NUMBER OF CHARACTERS IN len */
             /* THIS IS WHERE YOU CAN READ YOUR IMU, PRINT TO THE LCD, ETC */
-            len = sprintf(dataOut, "%d\r\n", i);
+            
+            
+
+            static signed short temperature;
+            static signed short gyroX;
+            static signed short gyroY;
+            static signed short gyroZ;
+            static signed short accelX;
+            static signed short accelY;
+            static signed short accelZ;
+
+            char m[100];
+            unsigned char data[14];
+
+            signed short who = readWho();
+
+            signed int templ = I2C_read(0b01101011, 0b00100000);
+            signed int temph = I2C_read(0b01101011, 0b00100001);
+            temperature = temph<<8 | templ;
+
+            signed int gyroXl = I2C_read(0b01101011, 0b00100010);
+            signed int gyroXh = I2C_read(0b01101011, 0b00100011);
+            gyroX = gyroXh<<8 | gyroXl;
+
+            signed int gyroYl = I2C_read(0b01101011, 0b00100100);
+            signed int gyroYh = I2C_read(0b01101011, 0b00100101);
+            gyroY = gyroYh<<8 | gyroYl;
+
+            signed int gyroZl = I2C_read(0b01101011, 0b00100110);
+            signed int gyroZh = I2C_read(0b01101011, 0b00100111);
+            gyroZ = gyroZh<<8 | gyroZl;
+
+            signed int accelXl = I2C_read(0b01101011, 0b00101000);
+            signed int accelXh = I2C_read(0b01101011, 0b00101001);
+            accelX = accelXh<<8 | accelXl;
+
+            signed int accelYl = I2C_read(0b01101011, 0b00101010);
+            signed int accelYh = I2C_read(0b01101011, 0b00101011);
+            accelY = accelYh<<8 | accelYl;
+
+            signed int accelZl = I2C_read(0b01101011, 0b00101100);
+            signed int accelZh = I2C_read(0b01101011, 0b00101101);
+            accelZ = accelZh<<8 | accelZl;
+
+            sprintf(m, "WHO_AM_I? %d", who); 
+            LCD_print(m, 28, 30, ILI9341_WHITE, ILI9341_BLACK);
+
+
+
+            sprintf(m, "Temperature: %5d", temperature); 
+            LCD_print(m, 28, 50, ILI9341_WHITE, ILI9341_BLACK);
+            sprintf(m, "gyroX: %5d", gyroX); 
+            LCD_print(m, 28, 60, ILI9341_WHITE, ILI9341_BLACK);
+            sprintf(m, "gyroY: %5d", gyroY); 
+            LCD_print(m, 28, 70, ILI9341_WHITE, ILI9341_BLACK);
+            sprintf(m, "gyroZ: %5d", gyroZ); 
+            LCD_print(m, 28, 80, ILI9341_WHITE, ILI9341_BLACK);
+
+            sprintf(m, "accelX: %5d", accelX); 
+            LCD_print(m, 28, 90, ILI9341_WHITE, ILI9341_BLACK);
+            sprintf(m, "accelY: %5d", accelY); 
+            LCD_print(m, 28, 100, ILI9341_WHITE, ILI9341_BLACK);
+            sprintf(m, "accelZ: %5d", accelZ); 
+            LCD_print(m, 28, 110, ILI9341_WHITE, ILI9341_BLACK);
+
+            signed int propX = accelX/170;
+            signed int propY = accelY/170;
+
+            if (propX>0){
+                int j;
+                int jj;
+                for (j = 0; j < propX; j++){
+                    for (jj = 0; jj < 5; jj++){
+                        LCD_drawPixel(120+j, 160+jj, ILI9341_BLUE);
+                    }
+                }
+
+                int k;
+                int kk;
+                for (k = propX; k < 100; k++){
+                    for (kk = 0; kk < 5; kk++){
+                        LCD_drawPixel(120+k, 160+kk, ILI9341_WHITE);
+                    }
+                }
+
+                int l;
+                int ll;
+                for (l = -100; l <0; l++){
+                    for (ll = 0; ll < 5; ll++){
+                        LCD_drawPixel(120+l, 160+ll, ILI9341_WHITE);
+                    }
+                }
+
+            }
+
+            if (propX<0){
+                int j;
+                int jj;
+                for (j = -100; j < propX; j++){
+                    for (jj = 0; jj < 5; jj++){
+                        LCD_drawPixel(120+j, 160+jj, ILI9341_WHITE);
+                    }
+                }
+
+                int k;
+                int kk;
+                for (k = propX; k < 0; k++){
+                    for (kk = 0; kk < 5; kk++){
+                        LCD_drawPixel(120+k, 160+kk, ILI9341_BLUE);
+                    }
+                }
+
+                int l;
+                int ll;
+                for (l = 0; l < 100; l++){
+                    for (ll = 0; ll < 5; ll++){
+                        LCD_drawPixel(120+l, 160+ll, ILI9341_WHITE);
+                    }
+                }
+            }
+
+
+            if (propY>0){
+                int j;
+                int jj;
+                for (j = 0; j < propY; j++){
+                    for (jj = 0; jj < 5; jj++){
+                        LCD_drawPixel(120+jj, 160+j, ILI9341_BLUE);
+                    }
+                }
+
+                int k;
+                int kk;
+                for (k = propY; k < 100; k++){
+                    for (kk = 0; kk < 5; kk++){
+                        LCD_drawPixel(120+kk, 160+k, ILI9341_WHITE);
+                    }
+                }
+
+                int l;
+                int ll;
+                for (l = -100; l <0; l++){
+                    for (ll = 0; ll < 5; ll++){
+                        LCD_drawPixel(120+ll, 160+l, ILI9341_WHITE);
+                    }
+                }
+
+            }
+
+            if (propY<0){
+                int j;
+                int jj;
+                for (j = -100; j < propY; j++){
+                    for (jj = 0; jj < 5; jj++){
+                        LCD_drawPixel(120+jj, 160+j, ILI9341_WHITE);
+                    }
+                }
+
+                int k;
+                int kk;
+                for (k = propY; k < 0; k++){
+                    for (kk = 0; kk < 5; kk++){
+                        LCD_drawPixel(120+kk, 160+k, ILI9341_BLUE);
+                    }
+                }
+
+                int l;
+                int ll;
+                for (l = 0; l < 100; l++){
+                    for (ll = 0; ll < 5; ll++){
+                        LCD_drawPixel(120+ll, 160+l, ILI9341_WHITE);
+                    }
+                }
+            }
+
+            while (!(startTime - _CP0_GET_COUNT() < 240000)) { 
+                        ;
+                    }
+            LATAINV = 0b10000;
+            
+                    
+            len = sprintf(dataOut, "%d %d %d %d %d %d %d\r\n", i, accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
             i++; // increment the index so we see a change in the text
             /* IF A LETTER WAS RECEIVED, ECHO IT BACK SO THE USER CAN SEE IT */
+           
+            /*
             if (appData.isReadComplete) {
                 USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
                         &appData.writeTransferHandle,
                         appData.readBuffer, 1,
                         USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
             }
-            /* ELSE SEND THE MESSAGE YOU WANTED TO SEND */
+            /* ELSE SEND THE MESSAGE YOU WANTED TO SEND
             else {
                 USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
                         &appData.writeTransferHandle, dataOut, len,
                         USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
                 startTime = _CP0_GET_COUNT(); // reset the timer for acurate delays
             }
+            */
+            if ((appData.readBuffer[0] = 'r') && (i < 101)) {
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle,
+                        dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                startTime = _CP0_GET_COUNT();
+            }
+            /* ELSE SEND THE MESSAGE YOU WANTED TO SEND */
+            else {
+                len = 1;
+                dataOut[0] = 0;
+                appData.readBuffer[0] = ' ';
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle, dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                i = 0;
+                startTime = _CP0_GET_COUNT(); // reset the timer for acurate delays
+            }
+            
             break;
 
         case APP_STATE_WAIT_FOR_WRITE_COMPLETE:
